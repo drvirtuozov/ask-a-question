3a2120c168e34ff7f635353cebca1be0205dd509
drvirtuozov <dr.virtuozov@ya.ru>
2017-02-24 17:21:23 +0300

    Protect signup & login forms from authorized users

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -7,12 +7,14 @@ import Login from './login/Login';
 import Questions from './inbox/Questions';
 import Profile from './profile/Profile';
 import requireAuth from '../utils/requireAuth';
+import onlyGuest from '../utils/onlyGuest';
+
 
 export default (
   <Route path="/" component={Container}>
     <IndexRoute component={Index} />
-    <Route path="signup" component={Signup} />
-    <Route path="login" component={Login} />
+    <Route path="signup" component={onlyGuest(Signup)} />
+    <Route path="login" component={onlyGuest(Login)} />
     <Route path="inbox" component={requireAuth(Questions)} />
     <Route path="*" component={Profile} />
   </Route>  
