92b41ed98fd1a0a67b601f91cbd899b7b31cda86
drvirtuozov <dr.virtuozov@ya.ru>
2017-06-04 01:21:05 +0300

    Add graphql error type

---
+++ graphql_types/error.go
@@ -0,0 +1,35 @@
+package graphql_types
+
+import (
+	"github.com/graphql-go/graphql"
+	"strings"
+)
+
+var Error = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Error",
+	Fields: graphql.Fields{
+		"field": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				text := strings.ToLower(p.Source.(error).Error())
+				fields := []string{
+					"username", "password", "email",
+				}
+
+				for _, v := range fields {
+					if strings.Contains(text, v) {
+						return v, nil
+					}
+				}
+
+				return nil, nil
+			},
+		},
+		"message": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return p.Source.(error).Error(), nil
+			},
+		},
+	},
+})
