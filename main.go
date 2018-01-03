a89f58d1631053d4e63ffa53dd61724cf02ac8d7
drvirtuozov <drvirtuozov@ya.ru>
2018-01-03 12:00:00 +0300

    Fix optionalJWTMiddleware

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+
 	"github.com/drvirtuozov/ask-a-question/handlers"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -15,13 +18,22 @@ func NewOptionalJWTMiddleware(skipper func(echo.Context) bool) echo.MiddlewareFu
 }
 
 var optionalAuth = NewOptionalJWTMiddleware(func(ctx echo.Context) bool {
+	var bodyBytes []byte
 	var params struct {
 		Anon bool `json:"anon" form:"anon" query:"anon"`
 	}
 
+	if ctx.Request().Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(ctx.Request().Body)
+	}
+
+	ctx.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
 	if err := ctx.Bind(&params); err != nil {
 		panic(err)
 	}
+
+	ctx.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
 	if err := ctx.Validate(params); err != nil {
 		ctx.JSON(400, handlers.NewErrResponse(err))
